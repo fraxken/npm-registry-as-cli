a3f48390f4272dfa696bc201af2879e77c80ce35
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-27 18:34:19 +0200

    feat: use slimio-spinner

--- bin/index.js
+++ bin/index.js
@@ -6,28 +6,48 @@ require("make-promises-safe");
 // Require Third-party Dependencies
 const sade = require("sade");
 const { get } = require("httpie");
-const { red, gray, cyan, white } = require("kleur");
+const { red, cyan, white, green } = require("kleur");
 const prettyjson = require("prettyjson");
+const Spinner = require("@slimio/async-cli-spinner");
 
 // CONSTANTS
 const NPM_REGISTRY = new URL("https://registry.npmjs.org");
 
 sade("npmreg <packageName> [version]", true)
     .version("1.0.0")
-    .action(async(packageName, version) => {
-        console.log(gray().bold(`\n > search for package ${cyan().bold(packageName)} in the npm registry!\n`));
-        const verDefined = typeof version === "string";
-
-        try {
-            const uri = new URL(`${packageName}/${verDefined ? version : ""}`, NPM_REGISTRY);
-            const { data } = await get(uri);
-            if (!verDefined) {
-                delete data.versions;
-            }
-            console.log(prettyjson.render(data));
-        }
-        catch (err) {
-            console.log(red().bold(`Failed to fetch JSON payload for package ${white().bold(packageName)}`));
-        }
-    })
+    .describe("Search for a given package in the npm registry (and return the raw data)")
+    .example("npmreg httpie")
+    .example("npmreg @slimio/is latest")
+    .action(main)
     .parse(process.argv);
+
+/**
+ * @async
+ * @function main
+ * @param {!string} packageName
+ * @param {string} [version]
+ * @returns {Promise<void>}
+ */
+async function main(packageName, version) {
+    console.log("");
+    const verDefined = typeof version === "string";
+    const spin = new Spinner({
+        prefixText: white().bold(`Searching for '${cyan().bold(packageName)}' on the npm registry`)
+    }).start("");
+
+    try {
+        const uri = new URL(`${packageName}/${verDefined ? version : ""}`, NPM_REGISTRY);
+        const { data } = await get(uri);
+        if (!verDefined) {
+            delete data.versions;
+        }
+
+        const elapsedTime = green().bold(`${spin.elapsedTime.toFixed(2)}ms`);
+        spin.succeed(white().bold(`Successfully fetched in ${elapsedTime}\n`));
+        console.log(prettyjson.render(data));
+    }
+    catch (err) {
+        spin.failed(red().bold(`Failed to fetch JSON payload for package ${white().bold(packageName)}`));
+        console.error(err);
+    }
+}
